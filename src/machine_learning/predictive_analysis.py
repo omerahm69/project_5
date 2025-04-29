70c3a360636341e4b1faf3f9d9dd8370aae8f388
omer ahmed <omerahm69@gmail.com>
2025-04-29 23:16:51 +0200

    Added model and  new plots

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 from tensorflow.keras.models import load_model
+
+
+
 from PIL import Image
 from src.data_management import load_pkl_file
 
